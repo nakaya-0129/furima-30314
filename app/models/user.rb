de1cf05c999ab34f6b57dc63e459e0094c05f1a0
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2020-11-07 10:47:36 +0900

    インデントの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,16 @@ class User < ApplicationRecord
 
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
          validates :password, format: {with: PASSWORD_REGEX ,message: "Include both letters and numbers"}, length: { minimum: 6 }
-         validates :email, uniqueness: {message: 'has already been taken'}, confirmation: true
+         validates :email, uniqueness: {message:'has already been taken'}, confirmation: true
 
         with_options presence: true do
            validates :nick_name, uniqueness: true 
            validates :birthday
-         with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message:'Full-width characters'} do
+         with_options format: {with:/\A[ぁ-んァ-ン一-龥]/,message:'Full-width characters'} do
            validates :surname
            validates :first_name
          end
-          with_options format: {with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'} do
+          with_options format: {with: /\A[ァ-ヶー－]+\z/,message:'Full-width katakana characters'} do
            validates :surname_kana
            validates :first_name_kana
          end
