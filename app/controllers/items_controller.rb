5145d8ac7592af2bbed3e55475a44a8911e6db75
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2020-11-13 18:42:22 +0900

    デバック用のコードの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
 
   def index
-    @items = Item.order("created_at DESC")
+    #@items = Item.order("created_at DESC")
   end
   
   def new
--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,12 +2,11 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
  priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  console.log(inputValue);
  const addTaxDom = document.getElementById("add-tax-price");
  addTaxDom.innerHTML = inputValue * 0.1;
- console.log(Math.floor(addTaxDom.innerHTML));
+ Math.floor(addTaxDom.innerHTML);
  const addProfit = document.getElementById("profit");
  addProfit.innerHTML = inputValue - addTaxDom.innerHTML
- console.log(Math.floor(addProfit.innerHTML));
+ Math.floor(addProfit.innerHTML);
 })
 });
