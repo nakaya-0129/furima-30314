f3036bbed2a1228e2f43cd982562cd762cabacfc
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2020-11-14 12:34:01 +0900

    不要コードの削除、修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
 
   def index
-    @items = Item.all
     @items = Item.order("created_at DESC")
   end
   
