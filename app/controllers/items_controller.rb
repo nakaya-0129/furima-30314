466226b70cfad5df0dfbefef140352083f9bd4c1
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2020-11-17 10:49:27 +0900

    editアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id
-      render :edit
-    else
+    if @item.user_id != current_user.id
       redirect_to root_path
     end
   end
