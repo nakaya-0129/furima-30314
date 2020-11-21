2456d3827bd1e4fd59265d896758aba17715d605
nakaya-0129 <71679288+nakaya-0129@users.noreply.github.com>
2020-11-21 11:03:28 +0900

    jsコード修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,7 +2,7 @@ const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) =>{
-    e.preventDefault()’
+    e.preventDefault()
 
     const formResult =document.getElementById("charge-form");
     const formData = new FormData(formResult);
